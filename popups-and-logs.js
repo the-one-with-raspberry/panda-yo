d9c9a4b4045bf8d8e969e2bd655d221692d11ee6
cipriancostin <ciprian.costin@gmail.com>
2020-12-17 15:02:01 +0200

    Added enhanced loggig

--- popups-and-logs.js
+++ popups-and-logs.js
@@ -5,5 +5,7 @@ function myFunction() {
   } else {
     txt = "Connection cancelled. It will not work.";
   }
+  console.log ("Now we are beginning to log");
   console.log (txt);
+  console.log ("We have finished logging");
 }
